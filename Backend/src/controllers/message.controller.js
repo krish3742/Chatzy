86e506fcff98732d0d1b499a4e04a6a2f325ec9b
krish3742 <krish3742@gmail.com>
2025-04-06 20:02:55 +0530

    Add API to fetch all messages between logged-in user and selected user

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getAllUsers = async (req, res, next) => {
   try {
@@ -10,6 +11,27 @@ export const getAllUsers = async (req, res, next) => {
       status: "success",
       message: "All user fetched",
       data: filteredUser,
+    };
+    res.status(200).json(resp);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const userToChatId = req.params.id;
+    const loggedInUserId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: loggedInUserId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: loggedInUserId },
+      ],
+    });
+    const resp = {
+      status: "success",
+      message: "All messages fetched",
+      data: messages,
     };
     res.status(200).json(resp);
   } catch (error) {
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { getAllUsers } from "../controllers/message.controller.js";
+import { getAllUsers, getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", verifyToken, getAllUsers);
+
+router.get("/:id", verifyToken, getMessages);
 
 export default router;
