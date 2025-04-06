089273377ce8662866eaa4bb8528e6b5db44aaae
krish3742 <krish3742@gmail.com>
2025-04-06 15:05:26 +0530

    Implemented basic auth routes and controllers

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
