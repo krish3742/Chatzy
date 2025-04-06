c512a4203dfcecb5a3d594e90fff45a4e3a2bf6d
krish3742 <krish3742@gmail.com>
2025-04-06 15:24:46 +0530

    Established MongoDB connection

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,24 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+const connectionString = process.env.MONGODB_URI;
+
+(async () => {
+  try {
+    await mongoose.connect(connectionString);
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
