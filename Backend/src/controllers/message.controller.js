002315262df062ad0742bccf408364f1edc71adb
krish3742 <krish3742@gmail.com>
2025-04-06 20:14:18 +0530

    Implemented API to send text or image messages between users

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import cloudinary from "../libs/cloudinary.js";
 import Message from "../models/message.model.js";
 
 export const getAllUsers = async (req, res, next) => {
@@ -34,6 +35,33 @@ export const getMessages = async (req, res, next) => {
       data: messages,
     };
     res.status(200).json(resp);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const sendMessage = async (req, res, next) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const receiverId = req.params.id;
+    const { text, image } = req.body;
+    const updateData = {
+      senderId: loggedInUserId,
+      receiverId,
+      text,
+    };
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      updateData.image = uploadResponse.secure_url;
+    }
+    const newMessage = new Message(updateData);
+    await newMessage.save();
+    const resp = {
+      status: "success",
+      message: "Message sent",
+      data: newMessage,
+    };
+    res.status(201).json(resp);
   } catch (error) {
     next(error);
   }
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,12 +1,18 @@
 import express from "express";
 
+import {
+  getAllUsers,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { getAllUsers, getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", verifyToken, getAllUsers);
 
 router.get("/:id", verifyToken, getMessages);
+
+router.post("/send/:id", verifyToken, sendMessage);
 
 export default router;
