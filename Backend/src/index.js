ce1207c9c6788f909bd18fa43de07a9d98447bb0
krish3742 <krish3742@gmail.com>
2025-04-07 12:36:46 +0530

    Configure CORS middleware to allow credentials

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -11,6 +12,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: `${process.env.CORS_ORIGIN_URL}`, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
