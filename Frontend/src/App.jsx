c110c17deae589e47753013063c0d9f51b6826e7
krish3742 <krish3742@gmail.com>
2025-04-07 11:58:08 +0530

    Set up basic routing and navigation with React Router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
